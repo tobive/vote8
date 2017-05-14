ed032fb9b2d447e3e823e9710b3034ac733d0aa0
haryus <knightsofkangouw@gmail.com>
2017-05-14 17:03:55 +0100

    fix url hash

--- server/database.js
+++ server/database.js
@@ -15,7 +15,7 @@ module.exports.getRandom = function(obj, callback) {
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function() {
     console.log("db connected");
-
+    
     callback();
   })
 }
@@ -26,27 +26,21 @@ module.exports.save = function(obj, callback) {
   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
   db.once('open', function() {
     console.log("db connected");
-    //let newCount = new Counter();
-    Counter.findOneAndUpdate({
-      query: { counter: 1 },
-      update: { $inc: { sequence: 1 } },
-      new: true
-    });
-    console.log("BANGSAT ")
-    let val = parseInt(1000);
-    obj.link = hashids.encode(val);
-    let newPoll = new Poll(obj);
-    let promise = newPoll.save(function (err) {
-      if(err) return console.error(err);
-    })
-    promise.then(function (newPoll) {
-      newUser.find(function (err, Poll) {
+    Counter.findOneAndUpdate(
+    { counter: 1},
+    { $inc: { sequence: 1 }},
+    {new: true},
+    function(err, count) {
+      if(err) console.error("ERROR TAEK: ", err);
+      console.log("BANGSAT " + count.sequence)
+      let val = parseInt(count.sequence);
+      obj.link = hashids.encode(val);
+      let newPoll = new Poll(obj);
+      let promise = newPoll.save(function (err) {
         if(err) return console.error(err);
-        console.log(Poll);
-        //mongoose.disconnect();
+        db.close();
       })
     });
-    db.close();
     callback();
   });
 }
