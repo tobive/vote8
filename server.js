fad3bcca283636a09e10e172b4c52f4ae6db64c8
tobive <knightsofkangouw@gmail.com>
2017-07-16 19:28:54 +0700

    refactor api server

--- server.js
+++ server.js
@@ -30,33 +30,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '/home/tobive/Project/vote8/dist', 'index.html'));
 });
 
-
-// app.post('/api/postnew', function (req, res) {
-//   console.log("from server.js: POSTNEW " + JSON.stringify(req.body));
-//   database.save(req.body, () => console.log("SAVED NEW POLL"));
-// });
-//
-// app.post('/api/postvote', function (req, res) {
-//   console.log("from server.js: POSTVOTE " + JSON.stringify(req.body));
-//   database.votepoll(req.body, () => console.log("VOTED!"));
-// });
-//
-// app.get('/api/getRandom', function (req, res) {
-//   database.getRandom(function(obj) {
-//     if(!obj) console.log("Data empty");
-//     //console.log("FROM SERVER JS: ", obj);
-//     res.json(obj);
-//   });
-// });
-//
-// app.get('/api/getLatest', function (req, res) {
-//   database.getLatest(function(obj) {
-//     if(!obj) console.log("Data Empty");
-//     //console.log("GETLATEST: ", obj);
-//     res.json(obj);
-//   });
-// });
-
 app.get('/poll/:id', function (req, res) {
   database.getLink(req.params.id, function(obj, callback) {
     if(!obj) {
