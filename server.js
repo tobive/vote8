edab62983898880267bddedb75a9f3a569f9a943
tobive <knightsofkangouw@gmail.com>
2017-07-16 19:27:34 +0700

    refactor api request

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var database = require('./server/database');
+var api = require('./server/routes/api-routes');
 const PORT = process.env.PORT || 8000;
 const xxx = require('./server/lib/server-app.js');
 
@@ -21,25 +22,40 @@ function renderFullPage(html) {
 }
 
 app.use(express.static(path.resolve(__dirname, '/home/tobive/Project/vote8/dist')));
+app.use(bodyParser.json());
+
+app.use('/api', api);
+
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '/home/tobive/Project/vote8/dist', 'index.html'));
 });
 
-app.get('/api/getRandom', function (req, res) {
-  database.getRandom(function(obj) {
-    if(!obj) console.log("Data empty");
-    //console.log("FROM SERVER JS: ", obj);
-    res.json(obj);
-  });
-});
 
-app.get('/api/getLatest', function (req, res) {
-  database.getLatest(function(obj) {
-    if(!obj) console.log("Data Empty");
-    //console.log("GETLATEST: ", obj);
-    res.json(obj);
-  });
-});
+// app.post('/api/postnew', function (req, res) {
+//   console.log("from server.js: POSTNEW " + JSON.stringify(req.body));
+//   database.save(req.body, () => console.log("SAVED NEW POLL"));
+// });
+//
+// app.post('/api/postvote', function (req, res) {
+//   console.log("from server.js: POSTVOTE " + JSON.stringify(req.body));
+//   database.votepoll(req.body, () => console.log("VOTED!"));
+// });
+//
+// app.get('/api/getRandom', function (req, res) {
+//   database.getRandom(function(obj) {
+//     if(!obj) console.log("Data empty");
+//     //console.log("FROM SERVER JS: ", obj);
+//     res.json(obj);
+//   });
+// });
+//
+// app.get('/api/getLatest', function (req, res) {
+//   database.getLatest(function(obj) {
+//     if(!obj) console.log("Data Empty");
+//     //console.log("GETLATEST: ", obj);
+//     res.json(obj);
+//   });
+// });
 
 app.get('/poll/:id', function (req, res) {
   database.getLink(req.params.id, function(obj, callback) {
@@ -71,17 +87,6 @@ app.get('/testtest', function (req, res) {
   });
   console.log("ISI OBJ " + test);
   res.send(renderFullPage(test));
-});
-
-app.use(bodyParser.json());
-app.post('/api/postnew', function (req, res) {
-  console.log("from server.js: POSTNEW " + JSON.stringify(req.body));
-  database.save(req.body, () => console.log("SAVED NEW POLL"));
-});
-
-app.post('/api/postvote', function (req, res) {
-  console.log("from server.js: POSTVOTE " + JSON.stringify(req.body));
-  database.votepoll(req.body, () => console.log("VOTED!"));
 });
 
 app.listen(PORT, function () {
---
+++ server/routes/api-routes.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var database = require('../database');
+
+router.post('/postnew', function (req, res) {
+  console.log("from server.js: POSTNEW " + JSON.stringify(req.body));
+  database.save(req.body, () => console.log("SAVED NEW POLL"));
+});
+
+router.post('/postvote', function (req, res) {
+  console.log("from server.js: POSTVOTE " + JSON.stringify(req.body));
+  database.votepoll(req.body, () => console.log("VOTED!"));
+});
+
+router.get('/getRandom', function (req, res) {
+  database.getRandom(function(obj) {
+    if(!obj) console.log("Data empty");
+    //console.log("FROM SERVER JS: ", obj);
+    res.json(obj);
+  });
+});
+
+router.get('/getLatest', function (req, res) {
+  database.getLatest(function(obj) {
+    if(!obj) console.log("Data Empty");
+    //console.log("GETLATEST: ", obj);
+    res.json(obj);
+  });
+});
+
+module.exports = router;
